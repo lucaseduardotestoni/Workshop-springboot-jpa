393bf8f8f7b03187a115539371edd659b1daca95
lucaseduardotestoni <testonilucas015@gmail.com>
2025-08-26 14:44:35 -0300

    Create orderStatus Enum

--- src/main/java/curso/java/workshopjava/entities/Order.java
+++ src/main/java/curso/java/workshopjava/entities/Order.java
@@ -1,30 +1,34 @@
 package curso.java.workshopjava.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import curso.java.workshopjava.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-     private Long id;
+    private Long id;
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssX", timezone = "UTC")
     private Instant moment;
 
-     @ManyToOne
-     @JoinColumn(name = "client_id")
-     private User client;
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+    private Integer orderStatus;
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus order, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(order);
     }
+
     public Order() {
     }
 
@@ -42,6 +46,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
