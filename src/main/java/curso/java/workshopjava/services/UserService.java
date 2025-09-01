a87ecabf32284c585495a90b7780b68f14fdd72c
lucaseduardotestoni <testonilucas015@gmail.com>
2025-09-01 20:53:35 -0300

    Exception handling - findById

--- src/main/java/curso/java/workshopjava/services/UserService.java
+++ src/main/java/curso/java/workshopjava/services/UserService.java
@@ -2,6 +2,7 @@ package curso.java.workshopjava.services;
 
 import curso.java.workshopjava.model.User;
 import curso.java.workshopjava.repositories.UserRepository;
+import curso.java.workshopjava.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
       Optional<User> user = userRepository.findById(id);
-      return user.get();
+      return user.orElseThrow(() -> new ResourceNotFoundException(id));
     }
     public User insert(User user) {
         return userRepository.save(user);
---
+++ src/main/java/curso/java/workshopjava/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package curso.java.workshopjava.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
